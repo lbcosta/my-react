018e494268685214a8bb9e5961d7b19fe28b6167
Leonardo Costa <lbcosta.dev@gmail.com>
2020-03-04 19:13:03 -0300

    Estado e Hooks implementados

--- index.tsx
+++ index.tsx
@@ -9,17 +9,47 @@ let React = {
   }
 };
 
-const App = () => (
-  <div className="react-2020">
-    <h1>Hello, person!</h1>
-    <input type="text" placeholder="name" />
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
-      omnis veniam id ipsum voluptatibus laudantium aliquid ab porro dicta
-      tempore!
-    </p>
-  </div>
-);
+const states = [];
+let stateCursor = 0;
+
+const useState = initialState => {
+  const FROZENCURSOR = stateCursor;
+  states[FROZENCURSOR] = states[FROZENCURSOR] || initialState;
+
+  const setState = newState => {
+    states[FROZENCURSOR] = newState;
+    rerender();
+  };
+
+  stateCursor++;
+
+  return [states[FROZENCURSOR], setState];
+};
+
+const App = () => {
+  const [name, setName] = useState("person");
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="react-2020">
+      <h1>Hello, {name}!</h1>
+      <input
+        value={name}
+        onchange={e => setName(e.target.value)}
+        type="text"
+        placeholder="name"
+      />
+      <h2>The count is: {count}</h2>
+      <button onclick={() => setCount(count + 1)}>+</button>
+      <button onclick={() => setCount(count - 1)}>-</button>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
+        omnis veniam id ipsum voluptatibus laudantium aliquid ab porro dicta
+        tempore!
+      </p>
+    </div>
+  );
+};
 
 const render = (reactElementOrStringOrNumber, container) => {
   if (["string", "number"].includes(typeof reactElementOrStringOrNumber)) {
@@ -48,6 +78,12 @@ const render = (reactElementOrStringOrNumber, container) => {
   }
 
   container.appendChild(actualDomElement);
+};
+
+const rerender = () => {
+  stateCursor = 0;
+  document.querySelector("#app").firstChild.remove();
+  render(<App />, document.querySelector("#app"));
 };
 
 render(<App />, document.querySelector("#app"));
